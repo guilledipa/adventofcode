7bad4611d89c167215e76da61ae7459497f26d7f
guilledipa <guilledipasquale@gmail.com>
2020-11-08 15:52:18 +1100

    Solution day 1

--- 2019/day/1/main.go
+++ 2019/day/1/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+var massFilePath = flag.String("mass_file_path", "input/modules_mass.txt", "Modules mass data file.")
+
+func fuelRequirement(mass int) int {
+	var fuel int
+	if mass <= 0 {
+		return fuel
+	}
+	fuel = (mass / 3) - 2
+	return fuel
+}
 
 func main() {
-	fmt.Println("TODO")
+	file, err := os.Open(*massFilePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	var totalFuel int
+	for scanner.Scan() {
+		mass, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Printf("unable to convert to integer: %v", err)
+			continue
+		}
+		totalFuel = totalFuel + fuelRequirement(mass)
+	}
+	fmt.Printf("Total fuel requirements: %v", totalFuel)
 }
