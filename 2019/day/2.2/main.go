ca61223f345ca8005d22462b458d0d3deb135007
guilledipa <guilledipasquale@gmail.com>
2020-12-19 20:07:14 +1100

    Good

--- 2019/day/2.2/main.go
+++ 2019/day/2.2/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"adventofcode/2019/day/2/computer"
 	"bufio"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"strconv"
-
-	"github.com/guilledipa/adventofcode/2019/day/2/computer"
 )
 
-var programPath = flag.String("program_path", "../2/input/program.txt", "Intcode program file.")
+var (
+	programPath = flag.String("program_path", "../2/input/program.txt", "Intcode program file.")
+)
 
 func main() {
 	file, err := os.Open(*programPath)
@@ -30,5 +31,17 @@ func main() {
 		}
 		program = append(program, instruction)
 	}
-	fmt.Println(computer.Compute(&program))
+
+	programBis := make(computer.Program, len(program))
+	for noun := 0; noun <= 99; noun++ {
+		for verb := 0; verb <= 99; verb++ {
+			_ = copy(programBis, program)
+			programBis[1] = noun
+			programBis[2] = verb
+			result := computer.Compute(&programBis)
+			if result[0] == 19690720 {
+				fmt.Printf("Answer: %02d%02d -- %v", noun, verb, result)
+			}
+		}
+	}
 }
