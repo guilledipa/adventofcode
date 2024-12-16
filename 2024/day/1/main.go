0f964155e0a1496ac7e1d363568596ef776d2f34
Guille Dipa <guilledipasquale@gmail.com>
2024-12-16 22:21:07 +1100

    Error handling

--- 2024/day/1/main.go
+++ 2024/day/1/main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"adventofcode/2024/day/1/locationids"
 	"fmt"
+	"log"
 )
 
 func main() {
 	locs := new(locationids.LocationIDs)
 	if err := locs.PopulateVectors("input/location_ids.txt"); err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 	fmt.Println(int(locs.ComputeTotalDistance()))
 }
